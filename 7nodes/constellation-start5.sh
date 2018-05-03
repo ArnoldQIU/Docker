79a56ff09dd995908380e181e94f3bf7728eee10
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-03 22:19:47 +0800

    k8s

--- 7nodes/constellation-start5.sh
+++ 7nodes/constellation-start5.sh
@@ -10,7 +10,7 @@ do
     cp "keys/tm$i.pub" "$DDIR/tm.pub"
     cp "keys/tm$i.key" "$DDIR/tm.key"
     rm -f "$DDIR/tm.ipc"
-    CMD="constellation-node --url=https://172.17.0.8:900$i/ --port=900$i --workdir=$DDIR --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://172.17.0.2:9001/"
+    CMD="constellation-node --url=https://35.232.206.68:900$i/ --port=900$i --workdir=$DDIR --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://35.184.23.185:9001/"
     echo "$CMD >> qdata/logs/constellation$i.log 2>&1 &"
     $CMD >> "qdata/logs/constellation$i.log" 2>&1 &
 done
